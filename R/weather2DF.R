56380839b81a8ce1e10f11ba626c1d7ea2dae7c7
Nelson Roque <nelsonroquejr@gmail.com>
2018-10-11 13:24:53 -0400

    comeon

--- R/weather2DF.R
+++ R/weather2DF.R
@@ -80,30 +80,31 @@ weather2df <- function(df,id='user_id',date_c='date',date_f='%Y/%m/%d',lat='gps_
       }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
       
       if(!exists('results.df')) {
-        results.df <- data.frame()
+        results.day <- data.frame()
       } else {
         # merge results with weather station info
         results.df <- merge(results.df, wsr, by.x=c("usaf_station","wban_station"), by.y=c("usaf","wban"))
+        
+        # isolate set of columns of interest              
+        results.slim <- results.df %>% select(date, time,
+                                              station_name,ctry,state,elev_m, begin, end, distance,
+                                              latitude.x, longitude.x, usaf_station, wban_station,
+                                              air_pressure, air_pressure_quality,
+                                              elevation, ceiling_height, ceiling_height_quality,
+                                              wind_code, wind_speed, wind_speed_quality, wind_direction, wind_direction_quality,
+                                              visibility_code, visibility_distance, visibility_distance_quality,
+                                              temperature, temperature_quality, temperature_dewpoint, temperature_dewpoint_quality)#,
+        #KC1_code, KC1_condition_code, KC1_extreme_temp_month, KC1_temp, KC1_temp_quality,
+        #AU2_precipitation_code, AU2_intensity_and_proximity_code,
+        #GA1_cloud_type_code, GA1_cloud_type_quality_code,
+        #GA2_cloud_type_code, GA2_cloud_type_quality_code,
+        #AA1_precipitation_liquid, AA1_period_quantity_hrs, AA1_depth, AA1_condition_quality, AA1_quality_code,
+        #AA2_precipitation_liquid, AA2_period_quantity_hrs, AA2_depth, AA2_condition_quality, AA2_quality_code)
+        
+        # only give data for date of record
+        results.day <- results.slim %>% filter(date == cur.date.s)
       }
-      
-      # isolate set of columns of interest              
-      results.slim <- results.df %>% select(date, time,
-                                            station_name,ctry,state,elev_m, begin, end, distance,
-                                            latitude.x, longitude.x, usaf_station, wban_station,
-                                            air_pressure, air_pressure_quality,
-                                            elevation, ceiling_height, ceiling_height_quality,
-                                            wind_code, wind_speed, wind_speed_quality, wind_direction, wind_direction_quality,
-                                            visibility_code, visibility_distance, visibility_distance_quality,
-                                            temperature, temperature_quality, temperature_dewpoint, temperature_dewpoint_quality)#,
-                                            #KC1_code, KC1_condition_code, KC1_extreme_temp_month, KC1_temp, KC1_temp_quality,
-                                            #AU2_precipitation_code, AU2_intensity_and_proximity_code,
-                                            #GA1_cloud_type_code, GA1_cloud_type_quality_code,
-                                            #GA2_cloud_type_code, GA2_cloud_type_quality_code,
-                                            #AA1_precipitation_liquid, AA1_period_quantity_hrs, AA1_depth, AA1_condition_quality, AA1_quality_code,
-                                            #AA2_precipitation_liquid, AA2_period_quantity_hrs, AA2_depth, AA2_condition_quality, AA2_quality_code)
-      
-      # only give data for date of record
-      results.day <- results.slim %>% filter(date == cur.date.s)
+  
     } # <END> else
     
     # bind weather and current record
