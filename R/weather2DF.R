1081506b2ba75ca68fb3bd1dd8d497a190891340
Nelson Roque <nelsonroquejr@gmail.com>
2018-10-23 13:47:50 -0400

    updated

--- R/weather2DF.R
+++ R/weather2DF.R
@@ -160,6 +160,11 @@ weather2df <- function(df,id='user_id',date_c='date',date_f='%Y/%m/%d',lat='gps_
    # weather.df
 
    # one step further, create daily summary
+  weather.df <- weather.df %>%
+    filter(temperature_quality != "9") %>%
+    group_by(id,DATE_SEARCH) %>%
+    mutate(temp.d = as.numeric(gsub("[[:punct:]]", " ", temperature))) %>%
+    summarise(min.temperature = min(temp.d))
   
   # filter records for day (or not) depending on function argument
   # if(!full) {
