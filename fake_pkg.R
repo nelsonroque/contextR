53d819247445414cf2e982cd45fef87ccf187085
Nelson Roque <nelsonroquejr@gmail.com>
2019-07-03 18:51:10 -0400

    now I need to wrap context.df calls into a function

--- fake_pkg.R
+++ fake_pkg.R
@@ -1,11 +1,9 @@
 # load packages
-library(keyring)
-library(lunar)
-library(tibble)
-library(digest)
-library(jsonlite)
-library(tidyverse)
-library(DescTools)
+library(tidyverse) # for everything dplyr
+library(DescTools) # zodiac dependency
+library(lunar) # all lunar data outside of darksky
+library(digest) # for hashing API to create unique key for merging
+library(jsonlite) # for interpreting JSON
 
 # ///////////////////////////////////////////////////////////////////////////////
 
@@ -14,13 +12,18 @@ DARK_SKY_API_KEY = "001ef0b8cd273a1551d5abe38a4b26a2"
 
 # ///////////////////////////////////////////////////////////////////////////////
 
-# create demo table of api calls to make
-geo.df <- data.frame(neg_affect = c(90,80),
-                     dob = c("1991-05-16","1991-04-04"),
-                     lat = c(40.7788,39.7788),
-                     lng = c(-77.84137,-77.84137),
-                     ts = c(1273687200,1273687200)) %>%
-  mutate(og_date = lubridate::as_datetime(ts)) %>%
+# create demo table of minimum necessary features
+og.df <- data.frame(survey_result = c(90,80), # example of 'real data'
+                     dob = c("1991-05-16","1991-04-04"), # optional
+                     lat = c(40.7788,39.7788), # necessary for getting weather (see conversions)
+                     lng = c(-77.84137,-77.84137), # necessary for getting weather (see conversions)
+                     ts = c(1273687200,1273687200)) %>% # necessary for getting weather (see conversions)
+  mutate(og_date = lubridate::as_datetime(ts)) # make sure to have ts in as.Date format for other calls
+
+# ///////////////////////////////////////////////////////////////////////////////
+
+# pull 'free' context, and prepare darksky API calls, just in case
+context.df <- og.df %>%
   mutate(lat_lng = create_lat_lng_str(lat,lng),
          zodiac = get_zodiac_sign_from_dob(dob),
          birth_season = get_season_from_date_v(dob),
@@ -30,25 +33,26 @@ geo.df <- data.frame(neg_affect = c(90,80),
   mutate(lunar_result = purrr::pmap(list(dob), get_lunar_context_from_date)) %>%
   unnest(lunar_result)
 
-# UP TO THIS POINT, ALL CONTEXT IS FREE, GOING FORWARD, see:
+# need multiple API calls? 
+# no problem, just add a separate mutate call
+# (dont forget the additional api_call_id)
+
+# ///////////////////////////////////////////////////////////////////////////////
+
+# UP TO THIS POINT, ALL CONTEXT IS FREE
+
+# ///////////////////////////////////////////////////////////////////////////////
+
+# GOING FORWARD, see:
+
 # -= DARKSKY API PRICING: https://darksky.net/dev
-weather.list <- get_weather_context(geo.df$api_call)
 
-weather.daily <- weather.list$daily %>% rename_all(
-  funs(
-    stringr::str_to_lower(.) %>%
-      stringr::str_replace_all(., '\\.', '_')
-  ))
+# get all weather data for each api call (returns list)
+weather.list <- get_weather_context(context.df$api_call)
 
-og.plus.weather.daily <- weather.daily %>%
-    inner_join(geo.df)
-
-  
-  
-sanitize_cols <- function(df) {
-  rdf <- df 
-  return(rdf)
-}
+# stitch daily data with it!
+og.plus.weather.daily <- weather.list$daily %>%
+    inner_join(context.df)
 
 # -= CENSUS API RESTRICTIONS: https://cran.r-project.org/web/packages/censusapi/vignettes/getting-started.html
 
